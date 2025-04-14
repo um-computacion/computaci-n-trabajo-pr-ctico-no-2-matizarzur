fad9712c52f2e2c0959370304e0357d64c8cf3e5
matizarzur <matiaszarzur2@gmail.com>
2025-04-14 19:35:40 -0300

    implementacion test frases palindromos

--- test/test_palindrome.py
+++ test/test_palindrome.py
@@ -6,6 +6,10 @@ class TestPalindrome(unittest.TestCase):
         self.assertTrue(is_palindrome("madam"))
         self.assertTrue(is_palindrome("racecar"))
         self.assertTrue(is_palindrome("level"))
-
+        
+    def test_phrase_palindromes(self):
+        self.assertTrue(is_palindrome("A man, a plan, a canal: Panama"))
+        self.assertTrue(is_palindrome("Was it a car or a cat I saw?"))
+        self.assertTrue(is_palindrome("No lemon, no melon"))
 if __name__ == '__main__':
     unittest.main()
